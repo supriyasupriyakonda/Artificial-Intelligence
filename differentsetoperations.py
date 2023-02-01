e37f3e2ffdac8baf1a42485c3075e861402b9836
supriyasupriyakonda <113300967+supriyasupriyakonda@users.noreply.github.com>
2023-02-01 08:32:40 -0800

    Add files via upload

---
+++ differentsetoperations.py
@@ -0,0 +1,8 @@
+E = {0, 2, 5, 6, 8};
+N = {1, 9, 6, 4, 2};
+
+# set union
+print("Union of E and N is",E | N)
+print("Intersection of E and N is",E & N)
+print("Difference of E and N is",E - N)
+print("Symmetric difference of E and N is",E ^ N)
